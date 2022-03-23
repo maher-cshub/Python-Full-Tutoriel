19725e6d70d7cc96a69121e4becfc96353c1e86c
maher-cshub <maherarab.1299@gmail.com>
2022-03-23 21:50:48 +0100

    2nd-commit

--- 1) Keywords-Comments-Variables-Datatypes/main.py
+++ 1) Keywords-Comments-Variables-Datatypes/main.py
@@ -4,8 +4,15 @@
 """
 
 
+"""
+------------------------KEYWORDS-COMMENTS-VARIABLES-DATATYPES-----------------------------------
+"""
 
-""" 1) comments """  
+
+""" 
+    (1) COMMENTS 
+    *) they are not executed (just we write them to describe the code)
+"""  
 
 # 1 line comment
 
@@ -16,37 +23,60 @@ comment
 """
 
 
-""" 2) python keywords """  
-import keyword
-print(keyword.kwlist)
+""" 
+    (2) KEYWORDS
+    *) Keywords are the reserved words in Python
+    *) We cannot use a keyword as a variable name, function name or any other identifier
+    *) the are case sensitive
+
+"""  
+
+import keyword #called module (learn it later)
+print("*********************(2) KEYWORDS ************************\n")#here print() is used to dispaly text ,\n to jump to new line 
+print(keyword.kwlist) #here print is used to display results on screen, ... 
+print("*********************************************************\n")
 
 
+""" 
+    3) DATA TYPES, VARIABLES, ....
+    *) variable is a location stored in memory, 
+    *) we give a variable a name (identifier) so we can access it
+    *) in python we dont need to mention the type of the variable when we declare it
+""" 
 
-""" 3) data types, variables, .... """ 
-#none
-nothing = None
+#None DATATYPE (means has no type yet)
+nothing = None  #here we declared a variable named nothing and we assign to it the value None 
 
-#float 
-f = 123456789.e3
 
-#integer
-i = 2
+#integer DATATYPE (like 1  0  2   100   1456    ....)
+i = 2 #here we declared a variable named i and we assign to it the value 2
 
-#boolean
-b = False
 
-#string
-str1="hello"
+#complex DATATYPE (like 1+2j  (1 is real part , 2 is the imaginary part))
+cpx = 4 + 2j #here we declared a variable named cpx and we assign to it the value 4 + 2j
+
+
+#float DATATYPE (like 11,3  20,3 ...)
+f = 123456789.e3 #here we declared a variable named f and we assign to it the value 123456789 * 10^(3)
+
+
+#boolean DATATYPE ( either True or False)
+b = False #here we declared a variable named b and we assign to it the value False
+
+
+#string DATATYPE (means text like "hello" 'hi' '''how are you''')
+str1="hello" 
 str2='hi'
 str3="""welcome"""
 
-#complex
-cpx = 4 + 2j
 
-print ("(",nothing,")" ,"of type ",type(nothing))
-print ("(",f,")" ,"of type ",type(f))
-print ("(",i,")" ,"of type ",type(i))
-print ("(",b,")" ,"of type ",type(b))
-print ("(",str1,")" ,"of type ",type(str1))
-print ("(",str2,")" ,"of type ",type(str2))
-print ("(",str3,")" ,"of type ",type(str3))
+print("*********************(3) DATATYPES-VARIABLES ************************\n")
+print ("Type of (",nothing,")" ,"is ",type(nothing))
+print ("Type of (",f,")" ,"is ",type(f))
+print ("Type of (",i,")" ,"is ",type(i))
+print ("Type of (",b,")" ,"is ",type(b))
+print ("Type of (",cpx,")" , "is ", type(cpx))
+print ("Type of (",str1,")" ,"is ",type(str1))
+print ("Type of (",str2,")" ,"is ",type(str2))
+print ("Type of (",str3,")" ,"is ",type(str3))
+print("*********************************************************\n")
