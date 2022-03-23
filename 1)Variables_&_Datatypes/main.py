be4d9fb715e94f195a2094b27bacac8b07d2fac4
maher-cshub <maherarab.1299@gmail.com>
2022-03-23 20:15:43 +0100

    1st-commit

--- 1)Variables_&_Datatypes/main.py
+++ 1)Variables_&_Datatypes/main.py
@@ -1,19 +1,1 @@
-"""
-------------------------PYTHON FULL COURSE-----------------------------------
------------------------------------------------------------------------------
-"""
 
-#THIS IS A 1-line comment
-
-"""
-THIS 
-IS 
-A
-MULTI-LINE 
-COMMENT
-"""
-
-#----------------------VARIABLES & DATATYPES---------------------------------
-
-#CREATING A VARIABLE 
-a = 5 #here we created a variable named a and give it value=5 
